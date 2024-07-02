f7442525351c2ee636a72b9953a5708d266c0396
jaesuk <brian.j.kwak@gmail.com>
2024-07-02 15:53:43 +0900

    diesmann et al.(1999) fig 1

---
+++ diesmann_1999/x86_64/inoise.c
@@ -0,0 +1,608 @@
+/* Created by Language version: 7.7.0 */
+/* NOT VECTORIZED */
+#define NRN_VECTORIZED 0
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "mech_api.h"
+#undef PI
+#define nil 0
+#include "md1redef.h"
+#include "section.h"
+#include "nrniv_mf.h"
+#include "md2redef.h"
+ 
+#if METHOD3
+extern int _method3;
+#endif
+
+#if !NRNGPU
+#undef exp
+#define exp hoc_Exp
+extern double hoc_Exp(double);
+#endif
+ 
+#define nrn_init _nrn_init__INoise
+#define _nrn_initial _nrn_initial__INoise
+#define nrn_cur _nrn_cur__INoise
+#define _nrn_current _nrn_current__INoise
+#define nrn_jacob _nrn_jacob__INoise
+#define nrn_state _nrn_state__INoise
+#define _net_receive _net_receive__INoise 
+#define kin kin__INoise 
+#define seed seed__INoise 
+ 
+#define _threadargscomma_ /**/
+#define _threadargsprotocomma_ /**/
+#define _threadargs_ /**/
+#define _threadargsproto_ /**/
+ 	/*SUPPRESS 761*/
+	/*SUPPRESS 762*/
+	/*SUPPRESS 763*/
+	/*SUPPRESS 765*/
+	 extern double *getarg();
+ static double *_p; static Datum *_ppvar;
+ 
+#define t nrn_threads->_t
+#define dt nrn_threads->_dt
+#define bias _p[0]
+#define bias_columnindex 0
+#define std _p[1]
+#define std_columnindex 1
+#define del _p[2]
+#define del_columnindex 2
+#define dur _p[3]
+#define dur_columnindex 3
+#define x _p[4]
+#define x_columnindex 4
+#define i _p[5]
+#define i_columnindex 5
+#define n _p[6]
+#define n_columnindex 6
+#define iamp _p[7]
+#define iamp_columnindex 7
+#define noise _p[8]
+#define noise_columnindex 8
+#define on _p[9]
+#define on_columnindex 9
+#define Dn _p[10]
+#define Dn_columnindex 10
+#define _g _p[11]
+#define _g_columnindex 11
+#define _nd_area  *_ppvar[0]._pval
+ 
+#if MAC
+#if !defined(v)
+#define v _mlhv
+#endif
+#if !defined(h)
+#define h _mlhh
+#endif
+#endif
+ 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+ static int hoc_nrnpointerindex =  -1;
+ /* external NEURON variables */
+ /* declaration of user functions */
+ static double _hoc_seed(void*);
+ static int _mechtype;
+extern void _nrn_cacheloop_reg(int, int);
+extern void hoc_register_prop_size(int, int, int);
+extern void hoc_register_limits(int, HocParmLimits*);
+extern void hoc_register_units(int, HocParmUnits*);
+extern void nrn_promote(Prop*, int, int);
+extern Memb_func* memb_func;
+ 
+#define NMODL_TEXT 1
+#if NMODL_TEXT
+static const char* nmodl_file_text;
+static const char* nmodl_filename;
+extern void hoc_reg_nmodl_text(int, const char*);
+extern void hoc_reg_nmodl_filename(int, const char*);
+#endif
+
+ extern Prop* nrn_point_prop_;
+ static int _pointtype;
+ static void* _hoc_create_pnt(Object* _ho) { void* create_point_process(int, Object*);
+ return create_point_process(_pointtype, _ho);
+}
+ static void _hoc_destroy_pnt(void*);
+ static double _hoc_loc_pnt(void* _vptr) {double loc_point_process(int, void*);
+ return loc_point_process(_pointtype, _vptr);
+}
+ static double _hoc_has_loc(void* _vptr) {double has_loc_point(void*);
+ return has_loc_point(_vptr);
+}
+ static double _hoc_get_loc_pnt(void* _vptr) {
+ double get_loc_point_process(void*); return (get_loc_point_process(_vptr));
+}
+ extern void _nrn_setdata_reg(int, void(*)(Prop*));
+ static void _setdata(Prop* _prop) {
+ _p = _prop->param; _ppvar = _prop->dparam;
+ }
+ static void _hoc_setdata(void* _vptr) { Prop* _prop;
+ _prop = ((Point_process*)_vptr)->_prop;
+   _setdata(_prop);
+ }
+ /* connect user functions to hoc names */
+ static VoidFunc hoc_intfunc[] = {
+ 0,0
+};
+ static Member_func _member_func[] = {
+ "loc", _hoc_loc_pnt,
+ "has_loc", _hoc_has_loc,
+ "get_loc", _hoc_get_loc_pnt,
+ "seed", _hoc_seed,
+ 0, 0
+};
+ /* declare global and static user variables */
+ /* some parameters have upper and lower limits */
+ static HocParmLimits _hoc_parm_limits[] = {
+ 0,0,0
+};
+ static HocParmUnits _hoc_parm_units[] = {
+ "bias", "nA",
+ "std", "nA",
+ "del", "ms",
+ "dur", "ms",
+ "n", "nA",
+ "i", "nA",
+ 0,0
+};
+ static double delta_t = 0.01;
+ static double n0 = 0;
+ static double v = 0;
+ /* connect global user variables to hoc */
+ static DoubScal hoc_scdoub[] = {
+ 0,0
+};
+ static DoubVec hoc_vdoub[] = {
+ 0,0,0
+};
+ static double _sav_indep;
+ static void nrn_alloc(Prop*);
+static void  nrn_init(NrnThread*, _Memb_list*, int);
+static void nrn_state(NrnThread*, _Memb_list*, int);
+ static void nrn_cur(NrnThread*, _Memb_list*, int);
+static void  nrn_jacob(NrnThread*, _Memb_list*, int);
+ static void _hoc_destroy_pnt(void* _vptr) {
+   destroy_point_process(_vptr);
+}
+ 
+static int _ode_count(int);
+static void _ode_map(int, double**, double**, double*, Datum*, double*, int);
+static void _ode_spec(NrnThread*, _Memb_list*, int);
+static void _ode_matsol(NrnThread*, _Memb_list*, int);
+ 
+#define _cvode_ieq _ppvar[2]._i
+ static void _ode_matsol_instance1(_threadargsproto_);
+ /* connect range variables in _p that hoc is supposed to know about */
+ static const char *_mechanism[] = {
+ "7.7.0",
+"INoise",
+ "bias",
+ "std",
+ "del",
+ "dur",
+ "x",
+ 0,
+ "i",
+ 0,
+ "n",
+ 0,
+ 0};
+ 
+extern Prop* need_memb(Symbol*);
+
+static void nrn_alloc(Prop* _prop) {
+	Prop *prop_ion;
+	double *_p; Datum *_ppvar;
+  if (nrn_point_prop_) {
+	_prop->_alloc_seq = nrn_point_prop_->_alloc_seq;
+	_p = nrn_point_prop_->param;
+	_ppvar = nrn_point_prop_->dparam;
+ }else{
+ 	_p = nrn_prop_data_alloc(_mechtype, 12, _prop);
+ 	/*initialize range parameters*/
+ 	bias = 0;
+ 	std = 0;
+ 	del = 0;
+ 	dur = 0;
+ 	x = 0;
+  }
+ 	_prop->param = _p;
+ 	_prop->param_size = 12;
+  if (!nrn_point_prop_) {
+ 	_ppvar = nrn_prop_datum_alloc(_mechtype, 3, _prop);
+  }
+ 	_prop->dparam = _ppvar;
+ 	/*connect ionic variables to this model*/
+ 
+}
+ static void _initlists();
+  /* some states have an absolute tolerance */
+ static Symbol** _atollist;
+ static HocStateTolerance _hoc_state_tol[] = {
+ 0,0
+};
+ extern Symbol* hoc_lookup(const char*);
+extern void _nrn_thread_reg(int, int, void(*)(Datum*));
+extern void _nrn_thread_table_reg(int, void(*)(double*, Datum*, Datum*, NrnThread*, int));
+extern void hoc_register_tolerance(int, HocStateTolerance*, Symbol***);
+extern void _cvode_abstol( Symbol**, double*, int);
+
+ void _inoise_reg() {
+	int _vectorized = 0;
+  _initlists();
+ 	_pointtype = point_register_mech(_mechanism,
+	 nrn_alloc,nrn_cur, nrn_jacob, nrn_state, nrn_init,
+	 hoc_nrnpointerindex, 0,
+	 _hoc_create_pnt, _hoc_destroy_pnt, _member_func);
+ _mechtype = nrn_get_mechtype(_mechanism[1]);
+     _nrn_setdata_reg(_mechtype, _setdata);
+ #if NMODL_TEXT
+  hoc_reg_nmodl_text(_mechtype, nmodl_file_text);
+  hoc_reg_nmodl_filename(_mechtype, nmodl_filename);
+#endif
+  hoc_register_prop_size(_mechtype, 12, 3);
+  hoc_register_dparam_semantics(_mechtype, 0, "area");
+  hoc_register_dparam_semantics(_mechtype, 1, "pntproc");
+  hoc_register_dparam_semantics(_mechtype, 2, "cvodeieq");
+ 	hoc_register_cvode(_mechtype, _ode_count, _ode_map, _ode_spec, _ode_matsol);
+ 	hoc_register_tolerance(_mechtype, _hoc_state_tol, &_atollist);
+ 	hoc_register_var(hoc_scdoub, hoc_vdoub, hoc_intfunc);
+ 	ivoc_help("help ?1 INoise /home/brian/Desktop/BCS_summer/neuron_tutorials/diesmann_1999/inoise.mod\n");
+ hoc_register_limits(_mechtype, _hoc_parm_limits);
+ hoc_register_units(_mechtype, _hoc_parm_units);
+ }
+static int _reset;
+static char *modelname = "Guassian-White noise";
+
+static int error;
+static int _ninits = 0;
+static int _match_recurse=1;
+static void _modl_cleanup(){ _match_recurse=1;}
+static int seed(double);
+ 
+static int _ode_spec1(_threadargsproto_);
+/*static int _ode_matsol1(_threadargsproto_);*/
+ static int _slist1[1], _dlist1[1];
+ static int kin(_threadargsproto_);
+ 
+static int  seed (  double _lx ) {
+   set_seed ( _lx ) ;
+    return 0; }
+ 
+static double _hoc_seed(void* _vptr) {
+ double _r;
+    _hoc_setdata(_vptr);
+ _r = 1.;
+ seed (  *getarg(1) );
+ return(_r);
+}
+ 
+/*CVODE*/
+ static int _ode_spec1 () {_reset=0;
+ {
+   noise = normrand ( 0.0 , std * 1.0 ) * 1.0 ;
+   Dn = ( - n + noise ) ;
+   }
+ return _reset;
+}
+ static int _ode_matsol1 () {
+ noise = normrand ( 0.0 , std * 1.0 ) * 1.0 ;
+ Dn = Dn  / (1. - dt*( ( - 1.0 ) )) ;
+  return 0;
+}
+ /*END CVODE*/
+ static int kin () {_reset=0;
+ {
+   noise = normrand ( 0.0 , std * 1.0 ) * 1.0 ;
+    n = n + (1. - exp(dt*(( - 1.0 ))))*(- ( ( noise ) ) / ( ( - 1.0 ) ) - n) ;
+   }
+  return 0;
+}
+ 
+static int _ode_count(int _type){ return 1;}
+ 
+static void _ode_spec(NrnThread* _nt, _Memb_list* _ml, int _type) {
+   Datum* _thread;
+   Node* _nd; double _v; int _iml, _cntml;
+  _cntml = _ml->_nodecount;
+  _thread = _ml->_thread;
+  for (_iml = 0; _iml < _cntml; ++_iml) {
+    _p = _ml->_data[_iml]; _ppvar = _ml->_pdata[_iml];
+    _nd = _ml->_nodelist[_iml];
+    v = NODEV(_nd);
+     _ode_spec1 ();
+ }}
+ 
+static void _ode_map(int _ieq, double** _pv, double** _pvdot, double* _pp, Datum* _ppd, double* _atol, int _type) { 
+ 	int _i; _p = _pp; _ppvar = _ppd;
+	_cvode_ieq = _ieq;
+	for (_i=0; _i < 1; ++_i) {
+		_pv[_i] = _pp + _slist1[_i];  _pvdot[_i] = _pp + _dlist1[_i];
+		_cvode_abstol(_atollist, _atol, _i);
+	}
+ }
+ 
+static void _ode_matsol_instance1(_threadargsproto_) {
+ _ode_matsol1 ();
+ }
+ 
+static void _ode_matsol(NrnThread* _nt, _Memb_list* _ml, int _type) {
+   Datum* _thread;
+   Node* _nd; double _v; int _iml, _cntml;
+  _cntml = _ml->_nodecount;
+  _thread = _ml->_thread;
+  for (_iml = 0; _iml < _cntml; ++_iml) {
+    _p = _ml->_data[_iml]; _ppvar = _ml->_pdata[_iml];
+    _nd = _ml->_nodelist[_iml];
+    v = NODEV(_nd);
+ _ode_matsol_instance1(_threadargs_);
+ }}
+
+static void initmodel() {
+  int _i; double _save;_ninits++;
+ _save = t;
+ t = 0.0;
+{
+  n = n0;
+ {
+   i = 0.0 ;
+   iamp = 0.0 ;
+   }
+  _sav_indep = t; t = _save;
+
+}
+}
+
+static void nrn_init(NrnThread* _nt, _Memb_list* _ml, int _type){
+Node *_nd; double _v; int* _ni; int _iml, _cntml;
+#if CACHEVEC
+    _ni = _ml->_nodeindices;
+#endif
+_cntml = _ml->_nodecount;
+for (_iml = 0; _iml < _cntml; ++_iml) {
+ _p = _ml->_data[_iml]; _ppvar = _ml->_pdata[_iml];
+#if EXTRACELLULAR
+ _nd = _ml->_nodelist[_iml];
+ if (_nd->_extnode) {
+    _v = NODEV(_nd) +_nd->_extnode->_v[0];
+ }else
+#endif
+ {
+#if CACHEVEC
+  if (use_cachevec) {
+    _v = VEC_V(_ni[_iml]);
+  }else
+#endif
+  {
+    _nd = _ml->_nodelist[_iml];
+    _v = NODEV(_nd);
+  }
+ }
+ v = _v;
+ initmodel();
+}}
+
+static double _nrn_current(double _v){double _current=0.;v=_v;{ {
+   at_time ( nrn_threads, del ) ;
+   at_time ( nrn_threads, del + dur ) ;
+   if ( t < del + dur  && t >= del ) {
+     iamp = bias + noise ;
+     }
+   else {
+     iamp = 0.0 ;
+     }
+   i = iamp ;
+   }
+ _current += i;
+
+} return _current;
+}
+
+static void nrn_cur(NrnThread* _nt, _Memb_list* _ml, int _type){
+Node *_nd; int* _ni; double _rhs, _v; int _iml, _cntml;
+#if CACHEVEC
+    _ni = _ml->_nodeindices;
+#endif
+_cntml = _ml->_nodecount;
+for (_iml = 0; _iml < _cntml; ++_iml) {
+ _p = _ml->_data[_iml]; _ppvar = _ml->_pdata[_iml];
+#if EXTRACELLULAR
+ _nd = _ml->_nodelist[_iml];
+ if (_nd->_extnode) {
+    _v = NODEV(_nd) +_nd->_extnode->_v[0];
+ }else
+#endif
+ {
+#if CACHEVEC
+  if (use_cachevec) {
+    _v = VEC_V(_ni[_iml]);
+  }else
+#endif
+  {
+    _nd = _ml->_nodelist[_iml];
+    _v = NODEV(_nd);
+  }
+ }
+ _g = _nrn_current(_v + .001);
+ 	{ _rhs = _nrn_current(_v);
+ 	}
+ _g = (_g - _rhs)/.001;
+ _g *=  1.e2/(_nd_area);
+ _rhs *= 1.e2/(_nd_area);
+#if CACHEVEC
+  if (use_cachevec) {
+	VEC_RHS(_ni[_iml]) += _rhs;
+  }else
+#endif
+  {
+	NODERHS(_nd) += _rhs;
+  }
+  if (_nt->_nrn_fast_imem) { _nt->_nrn_fast_imem->_nrn_sav_rhs[_ni[_iml]] += _rhs; }
+#if EXTRACELLULAR
+ if (_nd->_extnode) {
+   *_nd->_extnode->_rhs[0] += _rhs;
+ }
+#endif
+ 
+}}
+
+static void nrn_jacob(NrnThread* _nt, _Memb_list* _ml, int _type){
+Node *_nd; int* _ni; int _iml, _cntml;
+#if CACHEVEC
+    _ni = _ml->_nodeindices;
+#endif
+_cntml = _ml->_nodecount;
+for (_iml = 0; _iml < _cntml; ++_iml) {
+ _p = _ml->_data[_iml];
+ _nd = _ml->_nodelist[_iml];
+#if CACHEVEC
+  if (use_cachevec) {
+	VEC_D(_ni[_iml]) -= _g;
+  }else
+#endif
+  {
+	NODED(_nd) -= _g;
+  }
+  if (_nt->_nrn_fast_imem) { _nt->_nrn_fast_imem->_nrn_sav_d[_ni[_iml]] -= _g; }
+#if EXTRACELLULAR
+ if (_nd->_extnode) {
+   *_nd->_extnode->_d[0] += _g;
+ }
+#endif
+ 
+}}
+
+static void nrn_state(NrnThread* _nt, _Memb_list* _ml, int _type){
+Node *_nd; double _v = 0.0; int* _ni; int _iml, _cntml;
+#if CACHEVEC
+    _ni = _ml->_nodeindices;
+#endif
+_cntml = _ml->_nodecount;
+for (_iml = 0; _iml < _cntml; ++_iml) {
+ _p = _ml->_data[_iml]; _ppvar = _ml->_pdata[_iml];
+ _nd = _ml->_nodelist[_iml];
+#if EXTRACELLULAR
+ _nd = _ml->_nodelist[_iml];
+ if (_nd->_extnode) {
+    _v = NODEV(_nd) +_nd->_extnode->_v[0];
+ }else
+#endif
+ {
+#if CACHEVEC
+  if (use_cachevec) {
+    _v = VEC_V(_ni[_iml]);
+  }else
+#endif
+  {
+    _nd = _ml->_nodelist[_iml];
+    _v = NODEV(_nd);
+  }
+ }
+ v=_v;
+{
+ { error =  kin();
+ if(error){fprintf(stderr,"at line 64 in file inoise.mod:\n	\n"); nrn_complain(_p); abort_run(error);}
+ }}}
+
+}
+
+static void terminal(){}
+
+static void _initlists() {
+ int _i; static int _first = 1;
+  if (!_first) return;
+ _slist1[0] = n_columnindex;  _dlist1[0] = Dn_columnindex;
+_first = 0;
+}
+
+#if NMODL_TEXT
+static const char* nmodl_filename = "/home/brian/Desktop/BCS_summer/neuron_tutorials/diesmann_1999/inoise.mod";
+static const char* nmodl_file_text = 
+  "COMMENT\n"
+  ":Ornstein-Uhlenbeck process\n"
+  ":http://hines.med.yale.edu/phpBB/viewtopic.php?f=16&t=1308\n"
+  "\n"
+  ": 	dn=(-n+u)*dt/tc+s*dWt\n"
+  ":	tc is a time constant\n"
+  ":	u is the mean\n"
+  ":	dWt is the Wiener process\n"
+  ": s is a parameter for the 'noise intensity'.\n"
+  "\n"
+  ": Assuming u=0 and s=1/tc \n"
+  ":    dn/dt=(-n+zt)/tc\n"
+  ":	zt is a normal random variable with mean=0 and some variance\n"
+  ":	n is then just added to the total current\n"
+  "ENDCOMMENT\n"
+  "\n"
+  "TITLE Guassian-White noise\n"
+  "\n"
+  "NEURON {\n"
+  "	POINT_PROCESS INoise\n"
+  "	RANGE i, std, tau, bias, del, dur, x\n"
+  "	ELECTRODE_CURRENT i\n"
+  "}\n"
+  "\n"
+  "UNITS {\n"
+  "	(nA) = (nanoamp)\n"
+  "}\n"
+  "\n"
+  "PARAMETER {\n"
+  "	bias = 0 (nA)\n"
+  "	std = 0   (nA)\n"
+  "\n"
+  "	del	= 0    (ms)\n"
+  "	dur = 0   (ms)\n"
+  "	x = 0\n"
+  "}\n"
+  "\n"
+  "ASSIGNED {\n"
+  "	i (nA)\n"
+  "	iamp (nA)\n"
+  "	noise (nA)\n"
+  "	on (1)\n"
+  "}\n"
+  "\n"
+  "\n"
+  "INITIAL {\n"
+  "	i = 0\n"
+  "	iamp = 0\n"
+  "}\n"
+  "\n"
+  "STATE {\n"
+  "	n (nA) \n"
+  "}\n"
+  "\n"
+  "PROCEDURE seed(x) {\n"
+  "	set_seed(x)\n"
+  "}\n"
+  "\n"
+  "BREAKPOINT {\n"
+  "	at_time(del)\n"
+  "	at_time(del+dur)\n"
+  "	\n"
+  "	SOLVE kin METHOD cnexp\n"
+  "	\n"
+  "	if (t<del+dur && t>=del) { \n"
+  "		: iamp = bias \n"
+  "		iamp = bias + noise\n"
+  "	} else {\n"
+  "		iamp = 0\n"
+  "	}\n"
+  "	\n"
+  "	i = iamp\n"
+  "\n"
+  "}\n"
+  "\n"
+  "DERIVATIVE kin {\n"
+  "	noise = normrand(0,std*1(/nA))*1(nA)\n"
+  "	n' = (-n + noise)\n"
+  "	: n' = (-n + noise)\n"
+  "}\n"
+  ;
+#endif
